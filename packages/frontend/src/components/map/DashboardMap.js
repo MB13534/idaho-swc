e001807e3e72515dfb3b7063f3003d2fc7306457
MB13534 <michael.barry.email@gmail.com>
2022-01-21 17:04:52 -0700

    fix(DashboardMap): fixing logic for the coordinates lookup, will not recommend out of range cooridnates anymore

--- packages/frontend/src/components/map/DashboardMap.js
+++ packages/frontend/src/components/map/DashboardMap.js
@@ -107,21 +107,21 @@ const DashboardMap = ({
     const coord2 = Number(matches[2]);
     const geocodes = [];
 
-    if (coord1 < -90 || coord1 > 90) {
-      // must be lng, lat
-      geocodes.push(coordinateFeature(coord1, coord2));
-    }
-
-    if (coord2 < -90 || coord2 > 90) {
+    if (coord1 >= -90 && coord1 <= 90 && coord2 >= -180 && coord2 <= 180) {
       // must be lat, lng
+      geocodes.push(coordinateFeature(coord1, coord2));
+    }
+
+    if (coord2 >= -90 && coord2 <= 90 && coord1 >= -180 && coord1 <= 180) {
+      // must be lng, lat
       geocodes.push(coordinateFeature(coord2, coord1));
     }
 
-    if (geocodes.length === 0) {
-      // else could be either lng, lat or lat, lng
-      geocodes.push(coordinateFeature(coord1, coord2));
-      geocodes.push(coordinateFeature(coord2, coord1));
-    }
+    // if (geocodes.length === 0) {
+    //   // else could be either lng, lat or lat, lng
+    //   geocodes.push(coordinateFeature(coord1, coord2));
+    //   // geocodes.push(coordinateFeature(coord2, coord1));
+    // }
 
     return geocodes;
   };
